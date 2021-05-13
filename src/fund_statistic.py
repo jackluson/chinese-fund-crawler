c34f5acd5582a2f55e1371db1fb2124e9c7f0eb4
jackluson <18219112108@163.com>
2021-05-13 23:28:01 +0800

    feat: update more detail statistic

--- src/fund_statistic.py
+++ src/fund_statistic.py
@@ -22,10 +22,31 @@ if __name__ == '__main__':
 
     stock_top_list = each_statistic.all_stock_fund_count(
         quarter_index="2021-Q1",
-        filter_count=89)
-    print('2020-Q4 top 50 股票')
-    pprint(stock_top_list)
+        filter_count=90)
+    print('2020-Q4 top 100 股票')
+    # pprint(stock_top_list)
     print(len(stock_top_list))
+    filter_list = []
 
-    stock_quarter_count = each_statistic.item_stock_fund_count('海螺水泥')
-    print("stock_quarter_count", stock_quarter_count)
+    for stock in stock_top_list:
+        stock_name = stock[0]
+        stock_sum = stock[1]
+
+        stock_quarter_count_tuple = each_statistic.item_stock_fund_count(
+            stock_name)
+        last_count_tuple = stock_quarter_count_tuple[len(
+            stock_quarter_count_tuple) - 2]  # 选出上一个季度的
+        diff = stock_sum - last_count_tuple[0]
+
+        diff_percent = '{:.2%}'.format(
+            diff / last_count_tuple[0])
+        flag = '📈' if diff > 0 else '📉'
+        if diff == 0:
+            flag = '⏸'
+        item_tuple = (stock_name, stock_sum, last_count_tuple[0],
+                      diff, diff_percent, flag)
+        if not float(diff_percent.rstrip('%')) < -20:
+            filter_list.append(item_tuple)
+        print(item_tuple)
+    pprint(filter_list)
+    pprint(len(filter_list))
